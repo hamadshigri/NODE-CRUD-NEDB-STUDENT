02e5b4089e760621f701dfb01b7f7850b3f16e29
hamadshigri <hamad.shigri95@gmail.com>
2022-10-10 14:24:45 +0500

    Added endpoint for avg score & chec student in DB

--- students.js
+++ students.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const Database = require('nedb');
 const students = new Database({filename: 'database/students.db', autoload: true});
-
 // GET all students data from the db
 // Endpoint: /api/v2/students
 
@@ -24,24 +23,24 @@ router.get('/', async (req, res) => {
 // GET one student data by their ID
 // Endpoint: /api/v2/students/213243
 
-router.get('/:idVariable', async(req, res) => {
-    try {
-        await students.findOne({_id: req.params.idVariable}, (err, data) => {
-            if(err) {
-                res.status(500).json({message: "Error in the DB"});
-            }
-            if(data != null){
-                res.status(200).send(data);
-            }
-            else {
-                res.status(400).json({message: "Student with this ID does not exist"});
-            }
-        })
-    }
-    catch {
-        res.status(500).json({message: "Error in this API"})
-    }
-})
+// router.get('/:idVariable', async(req, res) => {
+//     try {
+//         await students.findOne({_id: req.params.idVariable}, (err, data) => {
+//             if(err) {
+//                 res.status(500).json({message: "Error in the DB"});
+//             }
+//             if(data != null){
+//                 res.status(200).send(data);
+//             }
+//             else {
+//                 res.status(400).json({message: "Student with this ID does not exist"});
+//             }
+//         })
+//     }
+//     catch {
+//         res.status(500).json({message: "Error in this API"})
+//     }
+// })
 
 // GET all student from particular city
 // Endpoint: /api/v2/students/city/karachi
@@ -65,19 +64,63 @@ router.get('/city/:cityVar', async(req, res) => {
     }
 })
 
-// Homework 1
+// Assignment Question 1
 // GET the average score for all the students
 // Endpoint: /api/v2/students/avgScore
 
 // POST students data to the db
 // Endpoint: /api/v2/students
 
+router.get('/avgScore', async(req, res) => {
+    var x = 0;
+    try {
+        await students.find({}, (err,data) => {
+            data.map((indexVal) => {
+                x += indexVal.score;
+            })
+            res.status(200).json({ "Average Score of the students are " :  x / data.length })
+        })
+    }
+    catch {
+        res.status(500).json({message: 'Error in this API'});
+    }
+})
+
+// Assignment Question 2
+// first check if student with this name is already in the database
+// If not, Add the student
+// If yes, tell frontend that this student name is already exists.
+
+router.post('/name/:name', async(req,res) => {
+    try
+    { 
+      await students.find({name: req.params.name}, (err,data) => {
+      if(err)
+      {
+        res.status(500).json({message:'Server Side Error: ' + Error});
+      }
+      if(data==false)
+      {
+        students.insert(req.body)
+        res.status(200).json({message: ('Student Added Successfully')});
+      }
+      else
+      {
+        res.status(200).json({message: ('Already Exist')});
+      }
+    })
+  }
+    catch(Error)
+    {
+       res.status(500).json({message:'Server Side Error: ' + Error});
+    }
+
+  })
+
+  
+
 router.post('/', async (req, res) => {
     try {
-        // Homework 2
-        // first check if student with this name is already in the database
-        // If not, Add the student
-        // If yes, tell frontend that this student name is already exists.
         await students.insert(req.body);
         res.status(200).json({message: "Student Added Successfully"});
     }
